a9bb1765c1a176d271957c40f7a5104efc3218b8
niels <nprins@pop-os.localdomain>
2020-12-22 10:17:22 +0100

    completed day 22

---
+++ src/main/scala/aoc2020/Day22.scala
@@ -0,0 +1,37 @@
+package aoc2020
+
+import utils.FileScanner
+
+import scala.annotation.tailrec
+
+object Day22 extends App {
+  val decks = FileScanner("src/main/resources/day-22.input").asString().split("\n\n").toVector.map(_.split('\n').toVector)
+
+  val player1 = decks.head.tail.map(_.toInt)
+  val player2 = decks.tail.head.tail.map(_.toInt)
+
+  @tailrec
+  def play(deck1: Vector[Int], deck2: Vector[Int]): (Int, Vector[Int]) = {
+    if (deck1.isEmpty) (2, deck2)
+    else if (deck2.isEmpty) (1, deck1)
+    else if (deck1.head < deck2.head) play(deck1.tail, deck2.tail :+ deck2.head :+ deck1.head)
+    else play(deck1.tail :+ deck1.head :+ deck2.head, deck2.tail )
+  }
+
+  println(s"solution 1: ${play(player1, player2)._2.reverse.zipWithIndex.map(x => x._1 * (x._2 + 1)).sum}")
+
+  def playRecursive(deck1: Vector[Int], deck2: Vector[Int], p1Hands: Set[Vector[Int]], p2Hands: Set[Vector[Int]]): (Int, Vector[Int]) = {
+    if (p1Hands.contains(deck1) || p2Hands.contains(deck2)) (1, Vector())
+    else if (deck1.isEmpty) (2, deck2)
+    else if (deck2.isEmpty) (1, deck1)
+    else if ((deck1.head <= deck1.tail.size) && (deck2.head <= deck2.tail.size)) {
+      val res = playRecursive(deck1.tail.take(deck1.head), deck2.tail.take(deck2.head), Set(), Set())
+      if (res._1 == 1) playRecursive(deck1.tail :+ deck1.head :+ deck2.head, deck2.tail, p1Hands + deck1, p2Hands + deck2)
+      else playRecursive(deck1.tail, deck2.tail :+ deck2.head :+ deck1.head, p1Hands + deck1, p2Hands + deck2)
+    }
+    else if (deck1.head < deck2.head) playRecursive(deck1.tail, deck2.tail :+ deck2.head :+ deck1.head, p1Hands + deck1, p2Hands + deck2)
+    else playRecursive(deck1.tail :+ deck1.head :+ deck2.head, deck2.tail, p1Hands + deck1, p2Hands + deck2)
+  }
+    println(s"solution 2: ${playRecursive(player1, player2, Set(), Set())._2.reverse.zipWithIndex.map(x => x._1 * (x._2 + 1)).sum}")
+
+}
